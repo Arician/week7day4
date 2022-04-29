7c1a246b629fff2771224d5cbbb90dcd901d7f2b
arician <arician@live.com>
2022-04-29 12:04:03 +0100

    comment changes

--- src/app.js
+++ src/app.js
@@ -11,14 +11,14 @@ const app=async(yargsObj)=>{
             await addAnime({title:yargsObj.title,protag:yargsObj.protag,protagVA:yargsObj.protagVA,studio:yargsObj.studio})
             console.log(`Successfully added ${yargsObj.title}`)
         }else if(yargsObj.list){
-            // list movie function
+            // list movie function. query and search optional. Any query not listed in queryKey will result in all listed
             if (queryKey[yargsObj.query]){
                 await getAnime({[queryKey[yargsObj.query]]:yargsObj.search})
             }else{
                 await getAnime()
             }
         }else if(yargsObj.update){
-            // update movies with filterObj and updateObj
+            // update movies with query and change. Can currently only search for movies to update by title
             if (queryKey[yargsObj.query]){
                 await updateAnime({title:yargsObj.title},{[queryKey[yargsObj.query]]:yargsObj.change})
                 console.log(`${yargsObj.title} updated`)
@@ -26,7 +26,7 @@ const app=async(yargsObj)=>{
                 console.log("Incorrect update query")
             }
         }else if(yargsObj.delete){
-            // delete movie function by title, might try to find update
+            // delete movie function. Must specify search field.
             if (queryKey[yargsObj.query]){
                 await deleteAnime({[queryKey[yargsObj.query]]:yargsObj.target})
             }else{"Missing deletion query"}
